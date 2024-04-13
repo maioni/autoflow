c20501fbcc8045b1c47b28ee1b32927ea37472f5
Daniel Maioni Araújo de Assis <sniper_dmaa@hotmail.com>
2024-04-13 15:19:49 +0100

    Semaforos alternando entre verde, amarelo e vermelho

--- src/simulator/devices.ts
+++ src/simulator/devices.ts
@@ -9,9 +9,21 @@ import express, { Request, Response } from "express";
 const semaphores: Semaphore[] = [];
 let timeAux = new Date();
 
+// Tempo de chegada de carros
+let carTime = 5000;
+
+// Tempo de cada cor do semáforo
+let redTime = 5000;
+let yellowTime = 10000;
+let greenTime = 15000;
+
+// Inicializa os semáforos
 export async function setupDevices() {
+
+  // Busca os semáforos
   await fetchSemaphores();
 
+  // Mostra os semáforos
   for (const semaphore of semaphores) {
     console.log(
       `${getColor(semaphore.description.split("-")[1] as any)}[•]${
@@ -24,57 +36,56 @@ export async function setupDevices() {
 
   console.log("\n");
 
+  // Inicializa os semáforos
   setInterval(() => {
     for (const semaphore of semaphores) {
       semaphore.carCount += Math.floor(Math.random() * semaphore.fator) + 1;
     }
-  }, 5000);
+  }, carTime);
 
-  updateSemaphoreStatus(); // Start the process
+  // Mostra os tempos
+  console.log(`Red Time: ${redTime}ms`);
+  console.log(`Yellow Time: ${yellowTime}ms`);
+  console.log(`Green Time: ${greenTime}ms`);
+  console.log("\n");
+
+  // Atualiza o status dos semáforos
+  updateSemaphoreStatus(); // Altera o tempo de cada cor do semáforo
 
 }
 
-let index = 0;
-
+// Atualiza o status dos semáforos
 function updateSemaphoreStatus() {
-  
-  
-  if (index >= semaphores.length) {
-    index = 0; // Reset the index if it's out of bounds
-  }
-
-  console.log("\nindex " + index + " para " + (semaphores.length - 1));
-
-  // Setar semaforo amarelo
-  semaphores[index].colorStatus = ColorStatus.YELLOW;
-
-  // Setar semaforo amarelo
-  if (index + 1 < semaphores.length) {
-    semaphores[index + 1].colorStatus = ColorStatus.GREEN;
-  } else {
-    semaphores[0].colorStatus = ColorStatus.GREEN;
-  }
-
-  // Setar semaforo vermelho
-  if (index - 1 >= 0) {
-    semaphores[index - 1].colorStatus = ColorStatus.RED;
-  } else {
-    semaphores[semaphores.length - 1].colorStatus = ColorStatus.RED;
-  } 
-
-  // Proximo semaforo
-  index++;
-
-  setTimeout(updateSemaphoreStatus, 10000); // Call this function again after 10 seconds
+  let currentIndex = 0;
+  setInterval(() => {
+    const semaphore = semaphores[currentIndex];
+    semaphore.colorStatus = ColorStatus.GREEN; // Set semaphore to green
+    setTimeout(() => {
+      semaphore.colorStatus = ColorStatus.YELLOW; // Set semaphore to yellow
+      setTimeout(() => {
+        semaphore.colorStatus = ColorStatus.RED; // Set semaphore to red
+        currentIndex = (currentIndex + 1) % semaphores.length; // Change to next semaphore
+      }, redTime); // Change red delay to 2000ms
+    }, yellowTime); // Change yellow delay to 3000ms
+  }, greenTime); // Change green delay to 10000ms
 }
 
+function setAllSemaphoresRed() {
+  for (const semaphore of semaphores) {
+    semaphore.colorStatus = ColorStatus.RED;
+  }
+}
+
+// Busca os semáforos
 async function fetchSemaphores() {
   const res = await fetch(
     "http://10.10.10.104:8000/discovery/resources?capability=semaphore"
   );
 
+  // Busca os recursos
   const resources = ((await res.json()) as any).resources as any;
 
+  // Adiciona os semáforos
   for (const resource of resources) {
     semaphores.push({
       uuid: resource.uuid,
@@ -88,6 +99,7 @@ async function fetchSemaphores() {
   }
 }
 
+// Atualiza o status dos semáforos
 setInterval(async () => {
   for (const semaphore of semaphores) {
     switch (semaphore.colorStatus) {
@@ -103,6 +115,7 @@ setInterval(async () => {
   }
 }, 500);
 
+// Trata o status verde
 async function handleGreen(semaphore: Semaphore) {
   if (timeAux.getTime() + 3000 < new Date().getTime()) {
     semaphore.carCount = Math.max(semaphore.carCount - 2, 0);
@@ -110,12 +123,14 @@ async function handleGreen(semaphore: Semaphore) {
   }
 }
 
+// Trata o status amarelo
 async function handleYellow(semaphore: Semaphore) {
   semaphore.carCount = semaphore.carCount > 0 ? semaphore.carCount - 1 : 0;
 }
 
+// Mostra o status dos semáforos
 console.log("STATUS DOS SEMAFOROS");
-
+// Mostra o status dos semáforos
 setInterval(() => {
   process.stdout.clearLine(0);
   process.stdout.cursorTo(0);
@@ -129,6 +144,7 @@ setInterval(() => {
   process.stdout.write(semaphoresStatus.join(" | "));
 }, 500);
 
+// Envia os dados dos semáforos
 setInterval(async () => {
   for (const semaphore of semaphores) {
     const body = {
@@ -151,13 +167,14 @@ setInterval(async () => {
   }
 }, 500);
 
+// Inicializa os semáforos
 setupDevices();
-
+// Inicializa o servidor
 const app = express();
 const port = 8000;
-
+// Habilita o uso de JSON
 app.use(express.json());
-
+// Habilita o uso de CORS
 app.post("/webhook/:uuid", (req: Request<{ uuid: string }>, res: Response) => {
   for (const semaphore of semaphores) {
     if (semaphore.uuid === req.params.uuid) {
@@ -170,7 +187,7 @@ app.post("/webhook/:uuid", (req: Request<{ uuid: string }>, res: Response) => {
   }
   res.status(200).send("Webhook recebido com sucesso!");
 });
-
+// Inicializa o servidor
 app.listen(port, () => {
   console.log(`RECEBIMENTO DE COMANDOS HABILITADOS\n`);
 });
